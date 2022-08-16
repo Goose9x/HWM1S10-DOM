5ba402340c8e88d372674ad3293d3ad68379e472
Goose9x <110024300+Goose9x@users.noreply.github.com>
2022-08-17 02:09:00 +0700

    Add files via upload

---
+++ HomeWork/app.js
@@ -0,0 +1,49 @@
+let div = document.getElementById("text");
+let div1 = document.getElementById("text1");
+let div2 = document.getElementById("text2");
+let btn = document.getElementById("btn");
+
+
+// Bài 1: ẩn vad xuất hiện khối 
+// btn.onclick = function (){
+//         if (div.style.opacity !== "0"){
+//             div.style.opacity = "0"
+//         }else {
+//             div.style.opacity = "1"
+//         }
+//     }
+
+// Bài 2: Đổi màu random 
+// btn.onclick = function (){
+//         let a = Math.floor(Math.random() * 256)
+//         let b = Math.floor(Math.random() * 256)
+//         let c = Math.floor(Math.random() * 256)
+//         let color = `${a},${b},${c}`
+//         console.log(color);
+//         div.style.backgroundColor = `rgb(${color})`
+//     }
+
+// Bài 3: Đổi nội dung 2 Box
+    // btn.onclick = function (){
+    //     temp = ""
+    //     console.log(div1.innerText);
+    //     console.log(div2.innerText);
+    //     temp = div1.innerText
+    //     div1.innerText = div2.innerText
+    //     div2.innerText = temp
+    // }
+
+// Bài 4: tăng font chữ
+
+// div.style.fontSize = "14px"
+// btn.onclick = function () {
+//     let font = Number(div.style.fontSize.replace("px",""))
+//     font = font + 1
+//     div.style.fontSize = font + "px"
+// }
+
+
+
+
+
+
